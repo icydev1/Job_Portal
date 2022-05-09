0b6645187c79ab6f834a38eb3d4500cffcbc8890
icydev1 <neejurajpoot15@gmail.com>
2022-05-09 23:21:59 +0530

    pull

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -29,6 +29,7 @@
             @include('layout.navbar')
 
 
+
         <!-- Navbar End -->
 
 
@@ -88,10 +89,118 @@
 
         <!-- Footer End -->
 
+        {{-- <a  class="nav-link" href="#login" data-toggle="modal"><i class="fa fa-sign-in"></i> Login</a>
+  <a  class="nav-link" href="#register" data-toggle="modal"><i class="fa fa-user-plus"></i> Register</a> --}}
+  <!-- Login Modal -->
+  <div class="modal fade" id="login">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content form-wrapper">
+        <div class="close-box" data-dismiss="modal">
+          <i class="fa fa-times fa-2x"></i>
+        </div>
+        <div class="container-fluid mt-5">
+          <form action="" method="post" id="LoginForm">
+            <div class="form-group text-center">
+              <h4>Login Form</h4>
+              <h6>Enter your credentials</h6>
+            </div>
+            <div class="form-group" style="position: relative;">
+              <label for="l_email">Email</label>
+              <input type="email" id="l_email" class="form-control mb-1" placeholder="someone@something.domain" required>
+
+
+
+            </div>
+            <div class="form-group pb-3" style="position: relative;">
+              <label for="l_password">Password</label>
+              <input type="password" id="l_password" class="form-control mb-1" placeholder="*******************" required>
+              <a href="#forgotPassword" style="display:block; position: absolute; right: 0;" title="Fill Email Field and Click it">
+                Forgot Password?
+              </a>
+            </div>
+            <div class="form-group pt-2">
+              <button class="btn btn-info form-control">Login</button>
+            </div>
+            <div class="form-group text-center pt-2 social-login">
+              <h6>OR Continue with</h6>
+              <a href="#" class="google"> <i class="fa fa-google-plus fa-lg"></i> </a>
+              <a href="#" class="facebook"> <i class="fa fa-facebook fa-lg"></i> </a>
+              <a href="#" class="twitter"> <i class="fa fa-twitter fa-lg"></i> </a>
+              <a href="#" class="github"> <i class="fa fa-github fa-lg"></i> </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Registeration Modal -->
+  <div class="modal fade" id="register">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content form-wrapper">
+        <div class="close-box" data-dismiss="modal">
+          <i class="fa fa-times fa-2x"></i>
+        </div>
+        <div class="container-fluid mt-5">
+          <form action="#" method="post" id="RegisterationForm">
+            <div class="form-group text-center pb-2">
+              <h4>Registration Form</h4>
+            </div>
+            <div class="form-row">
+              <div class="form-group col">
+                <label for="name">Name</label>
+                <input type="text" id="name" class="form-control" placeholder="Jhon Doe" required>
+              </div>
+              <div class="form-group col" style="position:relative;">
+                <label for='photo_upload' style="display:block">Profile Picture</label>
+                <button type="button" class="btn btn-dark form-control" onclick="document.getElementById('photo_upload').click()" id="photo_btn">Select Image</button>
+                <input type="file" name="photo" id="photo_upload" accept="image/*" style="display:none;" required>
+              </div>
+            </div>
+            <div class="form-group" style="position:relative;">
+              <label for="email">Email</label>
+              <input type="email" id="email" class="form-control mb-1" placeholder="jhonedoe@gmail.com" required>
+              <a href="#" data-toggle="modal" data-target="#login" style="display:none; position: absolute; right: 0; font-size: 12px;">That's you? Login</a>
+
+            </div>
+            <div class="form-row mb-1">
+              <div class="form-group col">
+                <label for="password">Password</label>
+                <input type="password" id="password" class="form-control" placeholder="*******************" required>
+
+              </div>
+              <div class="form-group col">
+                <label for="password_confirmation">Confirm Password</label>
+                <input type="password" id="password_confirmation" class="form-control" placeholder="*******************" required>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <button class="btn btn-info form-control">Register</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 
         <!-- Back to Top -->
         <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @include('layout.script')
 
