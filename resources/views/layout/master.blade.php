6b0d930ea9aa4eae93698551d31f3ceb387c096f
icydev1 <neejurajpoot15@gmail.com>
2022-05-09 21:09:36 +0530

    killer

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -10,21 +10,22 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
     <!-- Favicon -->
-   @include('layout.css')
+    @include('layout.css')
 </head>
 
 <body>
     <div class="container-xxl bg-white p-0">
         <!-- Spinner Start -->
-        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+        <div id="spinner"
+            class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; hEight'+x+': 3rem;" role="status">
                 <span class="sr-only">Loading...</span>
             </div>
         </div>
 
 
 
-            @include('layout.navbar')
+        @include('layout.navbar')
 
 
 
@@ -36,7 +37,7 @@
         <!-- Footer Start -->
 
 
-            @include('layout.footer')
+        @include('layout.footer')
 
 
         <!-- Footer End -->
@@ -51,3 +52,6 @@
 </body>
 
 </html>
+
+
+
