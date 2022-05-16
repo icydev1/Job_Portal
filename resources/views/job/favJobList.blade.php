9fa07045d53d6abaad2a26d6bf96a6a039030734
icydev1 <neejurajpoot15@gmail.com>
2022-05-16 22:57:40 +0530

    push

--- resources/views/job/favJobList.blade.php
+++ resources/views/job/favJobList.blade.php
@@ -4,7 +4,7 @@
 
     <div class="row">
 
-        <div class="col-md-10">
+        <div class="col-md-5">
             <!-- Item-->
             <div class="cart-item d-md-flex justify-content-between">
                 <div class="px-3 my-3">
@@ -14,13 +14,34 @@
                         <div class="cart-item-product-info">
                             <h2 class="cart-item-product-title">Tech Icy</h2>
                             <div class="text-lg text-body font-weight-medium pb-1"><h4>Job Shift</h4></div>
+                            <span class="sal-text">Category:<span class="text-success font-weight-medium">IT</span></span>
                             <span class="sal-text">Salary:<span class="text-success font-weight-medium">4000</span></span>
                         </div>
                     </a>
                 </div>
             </div>
         </div>
-        <div class="col-md-2 zoom">
+        <div class="col-md-1 zoom">
+            <span class="dynamic"><i class="fa fa-trash"></i></span>
+        </div>
+        <div class="col-md-5">
+            <!-- Item-->
+            <div class="cart-item d-md-flex justify-content-between">
+                <div class="px-3 my-3">
+                    <a class="cart-item-product" href="#">
+                        <div class="cart-item-product-thumb"><img src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                                alt="Product"></div>
+                        <div class="cart-item-product-info">
+                            <h2 class="cart-item-product-title">Tech Icy</h2>
+                            <div class="text-lg text-body font-weight-medium pb-1"><h4>Job Shift</h4></div>
+                            <span class="sal-text">Category:<span class="text-success font-weight-medium">IT</span></span>
+                            <span class="sal-text">Salary:<span class="text-success font-weight-medium">4000</span></span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-1 zoom">
             <span class="dynamic"><i class="fa fa-trash"></i></span>
         </div>
 
